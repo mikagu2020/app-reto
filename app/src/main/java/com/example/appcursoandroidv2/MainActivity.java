2f540da2fbf302bde302426e522a45fd50a5bf5f
mikagu <mikagu2020@gmail.com>
2020-06-02 15:33:06 +0200

    Dejado limpio MainActivity

--- app/src/main/java/com/example/appcursoandroidv2/MainActivity.java
+++ app/src/main/java/com/example/appcursoandroidv2/MainActivity.java
@@ -1,16 +1,8 @@
 package com.example.appcursoandroidv2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import com.example.appcursoandroidv2.dao.GastoDAOImpl;
-import com.example.appcursoandroidv2.beans.Gasto;
-import com.example.appcursoandroidv2.database.Conexion;
-import com.example.appcursoandroidv2.ui.inicio.InicioActivity;
-import com.example.appcursoandroidv2.utils.DateParser;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,16 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
     protected void setEventListeners() {
 
-
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //listarGatos();
-        intent = new Intent(this, InicioActivity.class);
-        startActivity(intent);
-    }
-
 
 }
